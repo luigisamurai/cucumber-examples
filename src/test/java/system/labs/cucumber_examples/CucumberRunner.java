ad962811f0a562521c9d30726e8eed8789ef1952
Luis Alberto Bolaños S <labs.sistemas@gmail.com>
2016-10-25 23:33:29 -0500

    Add path geckodriver

--- src/test/java/system/labs/cucumber_examples/CucumberRunner.java
+++ src/test/java/system/labs/cucumber_examples/CucumberRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"features"}, glue = {"stepDefination"})
+@CucumberOptions(features = "features", glue = {"system.labs.cucumber_examples.stepDefination"})
 public class CucumberRunner {
    
 }
